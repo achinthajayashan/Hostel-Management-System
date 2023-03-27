5ce4b3762ea3ac6d18a77c2024b58237f627559d
achinthajayashan <achintha.uns.2018@gmail.com>
2023-03-27 22:35:07 +0530

    appinitializer setuped

--- src/lk/ijse/d24hostelsystem/AppInitializer.java
+++ src/lk/ijse/d24hostelsystem/AppInitializer.java
@@ -1,4 +1,26 @@
 package lk.ijse.d24hostelsystem;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/d24hostelsystem/view/LoginForm.fxml"))));
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+    }
 }
