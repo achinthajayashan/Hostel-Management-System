83a72963ef6a1e07adf35cd851d2296940fa9804
achinthajayashan <achintha.uns.2018@gmail.com>
2023-03-28 12:40:22 +0530

    login page created

--- src/lk/ijse/d24hostelsystem/controller/LoginFormController.java
+++ src/lk/ijse/d24hostelsystem/controller/LoginFormController.java
@@ -1,4 +1,13 @@
 package lk.ijse.d24hostelsystem.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
 }
