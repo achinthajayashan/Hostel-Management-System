64d53197c706a8c242bdc58bb1c1b61242be42eb
achinthajayashan <achintha.uns.2018@gmail.com>
2023-03-31 12:59:28 +0530

    dashboard created

--- src/lk/ijse/d24hostelsystem/AppInitializer.java
+++ src/lk/ijse/d24hostelsystem/AppInitializer.java
@@ -17,9 +17,10 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/d24hostelsystem/view/LoginForm.fxml"))));
-        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/d24hostelsystem/view/DashboardForm.fxml"))));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
---
+++ src/lk/ijse/d24hostelsystem/controller/DashboardFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.d24hostelsystem.controller;
+
+public class DashboardFormController {
+}
